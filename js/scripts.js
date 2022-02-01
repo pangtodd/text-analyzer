d270497e92c358f9ef71cd065f85ed96d8091411
Todd Pangilinan <pang.todd@gmail.com>
2022-01-31 20:44:38 -0800

    added initial files

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+// Business Logic
+
+// function wordCounter(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(word) {
+//     wordCount++;
+//   });
+//   return wordCount;
+// }
+
+// function wordCounter(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(element) {
+//     if (!Number(element)) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+
+
+// function numberOfOccurrencesInText(word, text) {
+//   return 0;
+// }
+// function numberOfOccurrencesInText(word, text) {
+//   if (word === text) {
+//     return 1;
+//   }
+//   return 0;
+// }
+// function numberOfOccurrencesInText(word, text) {
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (word === element) {
+//       wordCount++
+//     }
+//   });
+//   return wordCount;
+// }
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (word === element) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (word.toLowerCase() === element.toLowerCase()) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
